b0db1795fc5c375535f06c91abb9616cf7b47795
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 22:40:10 +0000

    Snapshot of branch master taken on 2019-04-07T18:40:09.197-04:00 from "https://github.com/UnofficialJuliaMirror/JLSO.jl-9da8a3cd-07a3-59c0-a743-3fdc52c30d11"

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using BSON
+using Dates
+using Distributed
+using InteractiveUtils
+using Random
+using Serialization
+using Test
+using JLSO
+using Memento
+
+@testset "JLSO.jl" begin
+    include("JLSO.jl")
+end
